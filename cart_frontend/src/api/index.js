0e581a849526864e80e03f0084aaf585bbbe1983
guptasonu14 <sguptabudha@gmail.com>
2024-04-12 06:36:57 +0530

    modified

--- cart_frontend/src/api/index.js
+++ cart_frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axiosInstance from "axios";
 
-const url = process.env.REACT_APP_ENV === 'dev'? "http://localhost:5000" :"https://cart-backend.vercel.app" ;
+const url = process.env.REACT_APP_ENV === 'dev'? "http://localhost:3000" :"https://backend-cart-three.vercel.app/" ;
 
 const axios = axiosInstance.create({
     baseURL: url,
