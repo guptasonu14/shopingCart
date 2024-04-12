72750d31927db20f637b2e541a9770010e5112fb
guptasonu14 <sguptabudha@gmail.com>
2024-04-13 00:49:09 +0530

    modified

--- cart_frontend/src/api/contactService.js
+++
@@ -1,7 +0,0 @@
-import axios from ".";
-
-
-export const sendContactMessage = async (data) => {
-    const response = await axios.post("/api/contact/sendmessage", data);
-    return response.data;
-}
--- cart_frontend/src/api/products.js
+++
@@ -1,8 +0,0 @@
-import axios from ".";
-
-
-export const getProductsApi = async () => {
-    const response = await axios.get('/api/data/get/product');
-    console.log(response.data.data)
-    return response.data.data;
-}
